fc167d0d84810cc6692d9a55c1f715688ec2181d
Faizali Muhammadzoda <faizaliyusuf@gmail.com>
2023-10-25 20:21:17 +0400

    Added all files

---
+++ src/main/java/dev/faizali/deployyi/DeployyiApplication.java
@@ -0,0 +1,13 @@
+package dev.faizali.deployyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployyiApplication.class, args);
+	}
+
+}
